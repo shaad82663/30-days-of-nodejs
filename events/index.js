80ce676fdf3264ade69bbb898eb5bcd2921b4861
shaad82663 <shaad82663@gmail.com>
2023-05-04 16:04:35 +0530

    Added /'events'/ on 4th april

---
+++ events/index.js
@@ -0,0 +1,11 @@
+// https://www.youtube.com/watch?v=L18RHG2DwwA&list=PLC3y8-rFHvwj1_l8acs_lBi3a0HNb3bAN  -> Event Loop
+
+const EventEmitter = require('events')
+
+const event = new EventEmitter()
+
+event.on('open', (code) => {
+    console.log(`Event called with code ${code}`)
+})
+
+event.emit('open', 12)
