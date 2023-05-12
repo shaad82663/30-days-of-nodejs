a17646268f1c9771004b8b211c6767ec49b213ca
shaad82663 <shaad82663@gmail.com>
2023-05-12 11:27:12 +0530

    Edited day-29

--- day-29/index.js
+++ day-29/index.js
@@ -1,17 +1,12 @@
-//Name of the file : dropbox-file-upload.js
-//Including the required moduless
 var request = require('request');
 var fs = require('fs');
-
+require('dotenv').config({path : './config/.env'})
 //enter your access token
-var access_token = "sl.BeIhFsyEMAzTa5ymZ0FE8rvvBj5BHfofZyeo0haRW21erZdb_zTP0m2uVEz_QQ-7uo4cCrWH06MO8DK9h63VIp6pIItUNIKVP8UzmE2ZBFUgS-yv1ykavBPBHn2rjrYvrkA8JksV"
+var access_token = process.env.ACCESS_TOKEN
 //Name of the file to be uploaded
 var filename = 'myfile.txt';
 //reading the contents 
 var content = fs.readFileSync(filename);
-//write your folder name in place of YOUR_PATH_TO_FOLDER
-// For example if the folder name is njera then we can write it in the following way :
-// "Dropbox-API-Arg": "{\"path\": \"/njera/"+filename+"\",\"mode\": \"overwrite\",\"autorename\": true,\"mute\": false}"
 options = {
             method: "POST",
             url: 'https://content.dropboxapi.com/2/files/upload',
